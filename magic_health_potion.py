701ab21073496d4ee0bdc6e4ba313e141ab9980a
Emma Lundström <57397365+Emma278@users.noreply.github.com>
2020-01-05 14:10:42 +0100

    Update magic_health_potion.py

--- magic_health_potion.py
+++ magic_health_potion.py
@@ -1,19 +1,21 @@
-##for this project, I've been using Idle IDE on mac.
-##this is an imported module. Find modules from your Idle menu through help > pythondocs > modules.
-##the module has a .randint function that returns a random number between two set values. It looks like this: random.randint(x,y)
+## for this project, I've been using Idle IDE on mac.
+## the first row here is an imported module. Find modules from your Idle menu through help > pythondocs > modules.
+## the module has a .randint function that returns a random number between two set values. 
+## It looks like this: random.randint(x,y)
 import random
 
-##my game characters' initial health score.
+## my game characters' initial health score.
 health = 50
 
-##We're on the easiest level (difficulty one out of 3)
+## We're on the easiest level (difficulty one out of 3)
 difficulty = 1
 
-##My game character has encountered a health potion! The health potion's value is between 25 and 50 health points, divided by the difficulty level. 
+## My game character has encountered a health potion! The health potion's value is between 25 and 
+## 50 health points, divided by the difficulty level. 
 potion_health = int(random.randint(25,50) / difficulty)
 
-##My game character uses the potion and his health score increases accordingly.
+## My game character uses the potion and his health score increases accordingly.
 health = health + potion_health
 
-##This is the new health score!
+## This is the new health score!
 print(health)
