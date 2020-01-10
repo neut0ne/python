0b793993a9ae3478b69da322536d9d404f58eeda
Emma Lundström <57397365+Emma278@users.noreply.github.com>
2020-01-10 15:06:17 +0100

    Update cinema_simulator.py

--- cinema_simulator.py
+++ cinema_simulator.py
@@ -28,10 +28,12 @@ while True:
 
         num_seats = films[choice][1]
         
-        if num_seats > 0:
-            print("Enjoy the film!")
-            # subtract one seat from the chosen film
-            films[choice][1] = films[choice][1] - 1
+            if num_seats > 0:
+                print("Enjoy the film!")
+                # subtract one seat from the chosen film
+                films[choice][1] = films[choice][1] - 1
+            else: 
+                print("Sorry, we are sold out!")
         else:
             print("You are too young to see that film! Please choose a film within your age limit.")
         pass
