f51e96da3e4a5d867d3d8395464e3410b49b7d32
Gräviti <57397365+neut0ne@users.noreply.github.com>
2021-12-10 16:25:45 +0100

    Update emptytrash.py

--- emptytrash.py
+++ emptytrash.py
@@ -9,10 +9,8 @@ import sys
 os.chdir('/Users/emma/.Trash')
 if len(sys.argv) >= 2:
     if sys.argv[1] == '-t' or sys.argv[1] == '-T':
-	      print("option 1")
         os.system("tree ./")
     elif sys.argv[1] == '-l' or sys.argv[1] == '-L':
-	      print("option 2")
         os.system("ls -al")
 print("cleaning up trashbin...")
 os.system("rm -rf *")
